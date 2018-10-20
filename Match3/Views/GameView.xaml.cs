8f2b8b87a4aefb08f97039e51be77cbdd243c6ef
KeltarHelviett <keltarhelviett@gmail.com>
2018-10-20 19:38:13 +1000

    Add MainView laytout

---
+++ Match3/Views/GameView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Match3.Views
+{
+    public partial class GameView : Window
+    {
+        public GameView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Match3/Views/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using Match3.Views;
+
+namespace Match3
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayBtnClick(object sender, RoutedEventArgs e)
+        {
+            new GameView().Show();
+        }
+    }
+}
